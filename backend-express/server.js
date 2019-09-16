ab868d3e03f534d5cfe888196ad0320797b4dfa4
Jeon <James.Jeon@vce.com>
2019-09-16 15:54:23 -0500

    today last change

--- backend-express/server.js
+++ backend-express/server.js
@@ -2,9 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const app = express();
-
 app.get('/', (req, res) => res.send('API running'));
 
 const PORT = 5000;
-
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
