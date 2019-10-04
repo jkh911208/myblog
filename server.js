34a56133dbec5f08af35346ed9627adb7948bcf4
James Jeon <jkh911208@gmail.com>
2019-10-03 21:33:16 -0500

    remove frontend

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+app.get("/", (req, res) => res.send("API running!"));
+
+const PORT = 5000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
